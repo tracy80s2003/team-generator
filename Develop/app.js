4006599b58dba4a30454f5db7c048e3f39b25e7d
Tracy Golden <tracy80s2003@gmail.com>
2021-01-26 23:47:08 -0600

    added code to app.js and html files

--- Develop/app.js
+++ Develop/app.js
@@ -1,8 +1,10 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
+const Jest = require('jest');
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -10,64 +12,155 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-let employeeArr = [];
+const teamMembers = [];
+const employeeArr = [];
+const managerArr = [];
+const engineerArr = [];
+const internArr = [];
 
 //Whenever we call the render function that we are getting from our library we will use it as such: render(employeArr)
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-inquirer.prompt(
-    [
-        {
-            type: 'input',
-            message: "What is the employee name?",
-            name: "employeeName",
-            // validate property to check that the user provided a value
-            validate: (value) => { if(value){return true} else {return 'i need a value to continue'}},
+
+function managerData() {
+    inquirer.prompt([
+        {   
+            type: "input",
+            message: "Who is the manager?",
+            name: "managerName"
+        },
+        {   
+            type: "input",
+            message: "What is the manager's ID?",
+            name: "managerID"
+        },
+        {   
+            type: "input",
+            message: "What is the manager's email address?",
+            name: "managerEmail"
         },
         {
-            type: 'input',
-            message: "What is the employee ID number?",
-            name: "employeeID",
-            // validate property to check that the user provided a value
-            validate: (value) => { if(value){return true} else {return 'i need a value to continue'}},
-        },
-        {
-            type: 'input',
-            message: "What is the employee email address?",
-            name: "employeeEmail",
-            // validate property to check that the user provided a value
-            validate: (value) => { if(value){return true} else {return 'i need a value to continue'}},
-        },
-        {
-            type: 'input',
-            message: "What is the employee name?",
-            name: "employeeName",
-            // validate property to check that the user provided a value
-            validate: (value) => { if(value){return true} else {return 'i need a value to continue'}},
-        },
-    ])
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+            type: "input",
+            message: "What is the manager's office number?",
+            name: "officeNumber"
+        }]).then(managerAnswers => {
+            manager = new Manager(managerAnswers.managerName, managerAnswers.managerID, managerAnswers.managerEmail, managerAnswers.officeNumber);
+            teamTitle = managerAnswers.teamTitle;
+            console.log("Now we will ask for employee information.")
+            lesserEmployeeData();
+        });
+}
 
-// let render = function (template, node) {
-// 	if (!node) return;
-// 	node.innerHTML = template;
-// };
+function lesserEmployeeData() {
+    inquirer.prompt(
+        [
+            {
+                type: "list",
+                message: "What is this employee's role?",
+                name: "employeeRole",
+                choices: ["Intern", "Engineer", "Manager"],
+                // validate property to check that the user provided a value
+                validate: (value) => { if(value){return true} else {return 'i need a value to continue'}},
+            },            
+            {
+                type: 'input',
+                message: "What is the employee name?",
+                name: "employeeName",
+                // validate property to check that the user provided a value
+                validate: (value) => { if(value){return true} else {return 'i need a value to continue'}},
+            },
+            {
+                type: 'input',
+                message: "What is the employee ID number?",
+                name: "employeeId",
+                // validate property to check that the user provided a value
+                validate: (value) => { if(value){return true} else {return 'i need a value to continue'}},
+            },
+            {
+                type: 'input',
+                message: "What is the employee email address?",
+                name: "employeeEmail",
+                // validate property to check that the user provided a value
+                validate: (value) => { if(value){return true} else {return 'i need a value to continue'}},
+            },
+            {
+                type: "input",
+                message: "What is the Engineer's Github?",
+                name: "github",
+                when: (userInput) => userInput.employeeRole === "Engineer"
+            },
+            {
+                type: "input",
+                message: "What is the Interns school?",
+                name: "school",
+                when: (userInput) => userInput.employeeRole === "Intern"
+            },
+            {
+                type: "confirm",
+                name: "newEmployee",
+                message: "Would you like to add another team member?" 
+                // if yes, go back again. If no, renderHTML
+            }
+        ]).then(answers => {
+            if (answers.employeeRole === "Intern") {
+                const employee = new Intern(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.school);
+                teamMembers.push(employee);
+            } else if (answers.employeeRole === "Engineer") {
+                teamMembers.push(new Engineer(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.github));
+            }
+            if (answers.newEmployee === true) {
+                lesserEmployeeData();
+            } else {
+                let main = fs.readFileSync('./templates/main.html', 'utf8');
+                main = main.replace(/{{teamTitle}}/g, teamTitle);
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+                // Loop through employees to print out all of their cards without replacing a previous one.
+                let managerCard = fs.readFileSync('./templates/Manager.html', 'utf8');
+                managerCard = managerCard.replace('{{name}}', manager.getName());
+                managerCard = managerCard.replace('{{role}}', manager.getRole());
+                managerCard = managerCard.replace('{{id}}', manager.getId());
+                managerCard = managerCard.replace('{{email}}', manager.getEmail());
+                managerCard = managerCard.replace('{{officeNumber}}', manager.getOfficeNumber());
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+                var cards = managerCard; // Initial cards only has the Manager card info.
+                for (var i = 0; i < teamMembers.length; i++) {
+                    var employee = teamMembers[i];
+                    // Cards adds and then equals every new employee card info.
+                    cards += renderEmployee(employee);
+                }
+    
+                // Adds cards to main.html and outputs to team.html.
+                main = main.replace('{{cards}}', cards);
+    
+                fs.writeFileSync("team.html", htmlRenderer(res), err => {
+                    if (err) throw err;
+                });
+    
+                // Confirming that the team.html has been generated
+                console.log("The team.html has been generated!");
+            }
+        });
+    }
+    
+function renderEmployee(employee) {
+    if (employee.getRole() === "Intern") {
+        var internCard = fs.readFileSync('./templates/Intern.html', 'utf8');
+        internCard = internCard.replace('{{name}}', employee.getName());
+        internCard = internCard.replace('{{role}}', employee.getRole());
+        internCard = internCard.replace('{{id}}', employee.getId());
+        internCard = internCard.replace('{{email}}', employee.getEmail());
+        internCard = internCard.replace('{{school}}', employee.getSchool());
+        return internCard;
+    } else if (employee.getRole() === "Engineer") {
+        var engineerCard = fs.readFileSync('./templates/Engineer.html', 'utf8');
+        engineerCard = engineerCard.replace('{{name}}', employee.getName());
+        engineerCard = engineerCard.replace('{{role}}', employee.getRole());
+        engineerCard = engineerCard.replace('{{id}}', employee.getId());
+        engineerCard = engineerCard.replace('{{email}}', employee.getEmail());
+        engineerCard = engineerCard.replace('{{github}}', employee.getGithub());
+        return engineerCard;
+    }
+}
+    
+managerData();
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # team-generator
+
+## user story
+As a manager
+I want to generate a webpage that displays my team's basic info
+so that I have quick access to emails and GitHub profiles
